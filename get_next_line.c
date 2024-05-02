1477c5a8ff237560785169031473a138b47d5d12
hayesgr <42255528+hayesgr@users.noreply.github.com>
2024-05-01 19:23:05 -0500

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -22,50 +22,25 @@ char *get_next_line(int fd){
     return line;
 }
 
-typedef struct {
+char* get_next_line2(int fd){
     char *str;
-    int size;
-}mFile;
-
-mFile *read_file(int fd){
-    mFile *F;
-    F = malloc(sizeof(mFile));
-    F->str = malloc(1);
-    char * t = F->str;
-    if(!F->str){printf("malloc error\n");exit(0);}
     char buff;
     int n=0;
-    while (n=read(fd,&buff,1)>0){
-        F->size++;
-        *t=buff;
-        t++;
-        F->str = (char *)realloc(F->str,1);
+    str = malloc(1);
+    char *t = str;
+    int size=0;
+    while(n=read(fd,&buff,1)>0){
+        *t = buff;
+        if(buff=='\0')
+            break;
+        size++;
+        str = (char *)realloc(str,size+1);
+        if(!str){printf("realloc failed\n");exit(0);}
+        t = str+size;
+        if(buff=='\n')
+            break;
     }
-    F->size++;
+    if(n==0 && size==0){return NULL;}
     (*t)='\0';
-    return F;
-}
-char *get_next_line2(int fd){
-    static mFile *F;
-    static int index;
-    static int flag;
-    if(flag==0){
-        F = read_file(fd);
-        flag++;
-    }
-    if(flag==2){free(F->str);free(F);return NULL;}
-    int i=index,s=0;
-    char *ret;
-    while(F->str[i]!='\n' && F->str[i]!='\0'){
-        i++;s++;
-    }
-    if(F->str[i]!='\0'){i++;s++;}
-    if(F->str[i]=='\0'){flag++;}
-    ret = malloc(s+1);
-    for(int c=0;c<s;c++){
-        ret[c]=F->str[c+index];
-    }
-    ret[s]='\0';
-    index=i;
-    return ret;
+    return str;
 }
